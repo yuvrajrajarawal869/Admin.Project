d9ee73cd9c677d0cdd3b9196fe80adfbdd135cdd
yuvrajrajarawal869 <101393550+yuvrajrajarawal869@users.noreply.github.com>
2022-10-02 14:56:02 +0530

    day -4
    day -4 work

--- usecases/AdminLogin.java
+++ usecases/AdminLogin.java
@@ -13,37 +13,56 @@ public class AdminLogin {
 		String username = scanner.next();
 		
 		System.out.println("enter your password");
-		int password = scanner.nextInt();
+		String password = scanner.next();
 		AdminDao dao = new AdminDaoImpl();
 		
 		try {
 			String resultString = dao.AdminLogin(username, password);
 			System.out.println(resultString);
 			Scanner scanner2 = new Scanner(System.in);
-			System.out.println(2+".   enter for change password");
-			System.out.println(3+".   enter for enroll new course");
-			System.out.println(4+".   enter for registerd new faculty");
-			System.out.println(5+".   enter for get all course details");
+			System.out.println(1+".   enter for change password");
+			System.out.println(2+".   enter for Creat new Course course");
+			System.out.println(3+".   enter for Get All Course Details");
+			System.out.println(4+".   enter for Create new Batch");
+			System.out.println(5+".   enter for Get All Batch Details");
+			System.out.println(6+".   enter for RegisterFaculty new faculty");
+			System.out.println(7+".   enter for Get All Faculty Details");
+			System.out.println(8+".   enter for Create new Course Plan");
+			System.out.println(9+".   enter for see All Course Plan");
+			System.out.println(10+".   enter for Upadate Faculty PassWord");
 			int key = scanner2.nextInt();
 	    	switch (key) {
-			case 2:
+			case 1:
 				UpdatePasswprd.main(arg);
 				break;
-			case 3: 	
+			case 2:
 				CreatCourseByAdmin.main(arg);
+				break;
+			case 3: 	
+			    GetAllCourseDetails.main(arg);
 			    break;
 			case 4: 	
-				RegisterFaculty.main(arg);
+			    CreateBatchByAdmin.main(arg);
 			    break;
+			   
 			case 5: 	
-				GetAllCourseDetails.main(arg);
+			    GetAllBatchDetails.main(arg);
 			    break;
 			case 6: 	
+			    RegisterFaculty.main(arg);
+				   break;
+			case 7: 	
 				GetAllFacultyDetails.main(arg);
 			    break; 
-			case 7: 	
-				CreateBatchByAdmin.main(arg);
-			    break; 
+			case 8: 	
+				CreateCoursePlanByAdmin.main(arg);
+			    break;
+            case 9: 	
+				GetAllCoursePlan.main(arg);
+			    break;
+            case 10: 	
+				UpadateFacultyPassWord.main(arg);
+ 			    break;
 			default:
 				System.out.println("Please inter the write option");
 				break;
--- usecases/FacultyLogin.java
+++ usecases/FacultyLogin.java
@@ -13,15 +13,30 @@ public class FacultyLogin {
 		   String username = scanner.next();
 		   
 		   System.out.println("enter new your password");
-		   int passwrod = scanner.nextInt();
+		   String passwrod = scanner.next();
 		  
 		   AdminDao dao = new AdminDaoImpl();
 		   try {
 			   String result = dao.FacultyLogin(username,passwrod);	
 			   System.out.println(result);
-			   GetAllCoursePlan.main(arg);
-			   CreateCoursePlanByAdmin.main(arg);
-			   UpadateFacultyPassWord.main(arg);
+			   System.out.println(1+".  enter for see the course datails");
+			   System.out.println(2+".  enter for Fill up the day wise planne");
+			   System.out.println(3+".  enter for change password");
+			   int key = scanner.nextInt();
+		    	switch (key) {
+				case 1:
+					GetAllCoursePlan.main(arg);
+					break;
+                case 2:
+					CreateCoursePlanByAdmin.main(arg);
+					break;
+                case 3:
+					UpadateFacultyPassWord.main(arg);
+					break;
+				default:
+					System.out.println("Please inter the write option");
+					break;
+				}
 		} catch (Exception e) {
 		  System.out.println(e.getMessage());	
 		}
